8794f335248bd2c5b4be2b8da400c3191c731564
staredelweis <staredelweis@gmail.com>
2013-09-25 11:48:13 +0200

    Update intercambiodos variables.groovy

--- src/main/groovy/intercambiodos variables.groovy
+++ src/main/groovy/intercambiodos variables.groovy
@@ -1,22 +1,22 @@
 //
-    def readln=javax.swing.JOptionPane.&showInputDialog
-//Declaración de variables, debemos declarar 3 variables a y b para intercanviar 
-//y c  para guardar el valor de la variable a intercanviar  
-    Double a,b,c
+        def readln=javax.swing.JOptionPane.&showInputDialog
+//Declaración de variables, debemos declarar 3 variables a y b para intercanviar y c  para guardar el valor 
+//de la variable a intercanviar  
+        Double a,b,c
 //
-    a=Double.parseDouble(readln('Contenido variable a'))
-    b=Double.parseDouble(readln('Contenido variable b'))
+        a=Double.parseDouble(readln('Contenido variable a'))
+        b=Double.parseDouble(readln('Contenido variable b'))
 //asignamos el valor de "a" a la variable c para realizar el intercambio entre a y b
-    c=a
+        c=a
 //intercambiamos el valor de a por el valor de b
-    a=b
+        a=b
 //introducimos que valor tiene la variable a   
-    println 'Contenido variable a es'
+        println 'Contenido variable a es'
 //mostramos por pantalla el valor de la variable a   
-    println  a
+        println  a
 //intercambiamos el valor de la variable b     
-    b=c
+        b=c
 //introducimos que valor tiene la variable b    
-    println'Contenido variable b es'
+        println'Contenido variable b es'
 //mostramos por pantalla el valor de la variable b    
-    println b
+        println b
